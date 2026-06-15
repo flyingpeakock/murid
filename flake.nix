d810290f07f5f106d384744c328e72eec95efb63
Philip Johansson <philip@phlipphlop.me>
2026-06-15 18:11:08 +0200

    v0.0.8

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         }
         // {
           pname = "HardcoverHarvester";
-          version = "0.0.7";
+          version = "0.0.8";
 
           nativeCheckInputs = [
             pkgs.python3Packages.pytestCheckHook
