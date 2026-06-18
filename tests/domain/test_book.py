49fe1ab106eaf9c21f187f6f67b8a6d51a41e475
Philip Johansson <philip@phlipphlop.me>
2026-06-18 17:23:16 +0200

    add tests for book.py

---
+++ tests/domain/test_book.py
@@ -0,0 +1,24 @@
+from murid import Book
+
+
+def test_book_str():
+    book = Book(
+        title="Dune",
+        authors=["Frank Herbert"],
+        id=1,
+        isbn=[],
+        source="test",
+    )
+
+    assert str(book) == "Dune by Frank Herbert"
+
+def test_book_str_multiple_authors():
+    book = Book(
+        title="Good Omens",
+        authors=["Terry Pratchett", "Neil Gaiman"],
+        id=1,
+        isbn=[],
+        source="test",
+    )
+
+    assert str(book) == "Good Omens by Terry Pratchett, Neil Gaiman"
