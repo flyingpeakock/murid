4f5a2769abe3b9304ef5f0619ad15d3fb88e6906
Philip Johansson <philip@phlipphlop.me>
2026-06-18 18:57:56 +0200

    v0.1.2

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         }
         // {
           pname = "murid";
-          version = "0.1.1";
+          version = "0.1.2";
 
           nativeCheckInputs = [
             pkgs.python3Packages.pytestCheckHook
